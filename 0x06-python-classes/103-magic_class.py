73a506a24a8f6ddea3abc2042494b7ce5292975d
Nicholas Naidu <naidunicholas@gmail.com>
2024-01-23 09:06:04 -0800

    tasks

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,29 @@
+90% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for R 34.99 R 8.99/month for 3 months.
+#!/usr/bin/python3
+
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
+
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
